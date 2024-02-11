90d8f4f6795ff3758c32abe1c83fd7cb44d05d17
huzaifmalik786 <huzaif@heliverse.com>
2024-02-11 14:15:37 +0530

    changes

--- scripts/generate-robots-txt.js
+++ scripts/generate-robots-txt.js
@@ -1,14 +1,12 @@
 const fs = require("fs");
 
-const crawlableRobotsTxt = `User-agent: *\nDisallow: //scrum.\nDisallow: //register.\nDisallow: //certify.\nDisallow: //safe.\nDisallow: //media.\nDisallow: //schedule.\nDisallow: /_next/static/media/\nDisallow: /wp-admin/\nDisallow: /wp-includes/\nDisallow: /wp-content/plugins/\nDisallow: /wp-content/themes/\nDisallow: /wp-login.php\nDisallow: /wp-register.php\nDisallow: /xmlrpc.php\n\nSitemap: https://skillbookacademy.com/sitemap.xml`;
-
 const uncrawlableRobotsTxt = `User-agent: *\nDisallow: /`;
 
 function generateRobotsTxt() {
     // Create a non-crawlable robots.txt in non-production environments
     const robotsTxt =
         process.env.ENABLE_SEO === "true"
-            ? crawlableRobotsTxt
+            ? uncrawlableRobotsTxt
             : uncrawlableRobotsTxt;
 
 
